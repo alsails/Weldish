e9d4876df71093e97df95020eaf65b70e12787bc
Your Name <mash.pogonina2014@yandex.ru>
2023-04-03 23:45:39 +0300

    fix: редактировать категории товаров

--- src/components/CategorySlider.js
+++ src/components/CategorySlider.js
@@ -1,66 +1,93 @@
 import React from "react"
-import shortid  from "shortid"
+import shortid from "shortid"
 
 import CategoryTitle from "./CategoryTitle";
 import PreviewCard from "./PreviewCard";
 
-import {goods} from "../utils/goods";
+import { goods } from "../utils/goods";
 
 import styles from "../styles/categorySlider.module.scss"
 
 function CategorySlider() {
     var sale__goods = goods.filter(function (item) {
-        return item.sale 
+        return item.sale
     })
 
     var noSale__goods = goods.filter(function (item) {
-        return !item.sale 
+        return !item.sale
     })
 
     return (
         <section className={styles.category}>
-            <CategoryTitle title="Скидки" />
-            <ul className={styles.cards}>
-                {sale__goods.slice(0, 3).map(item => {
-                    return <li key={shortid.generate()}><PreviewCard item={item} sale="true"/></li>
-                })}
-            </ul>
-            <CategoryTitle title="Посуда, кухонный принадлежности" />
-            <ul className={styles.cards}>
-                {noSale__goods.slice(0, 3).map(item => {
-                    return <li key={shortid.generate()}><PreviewCard item={item} sale="false"/></li>
-                })}
-            </ul>
-            <CategoryTitle title="Бытовая техника для кухни" />
-            <ul className={styles.cards}>
-                {noSale__goods.slice(0, 3).map(item => {
-                    return <li key={shortid.generate()}><PreviewCard item={item} sale="false"/></li>
-                })}
-            </ul>
-            <CategoryTitle title="Бытовая химия" />
-            <ul className={styles.cards}>
-                {noSale__goods.slice(0, 3).map(item => {
-                    return <li key={shortid.generate()}><PreviewCard item={item} sale="false"/></li>
-                })}
-            </ul>
-            <CategoryTitle title="Хозяйственные товары" />
-            <ul className={styles.cards}>
-                {noSale__goods.slice(0, 3).map(item => {
-                    return <li key={shortid.generate()}><PreviewCard item={item} sale="false"/></li>
-                })}
-            </ul>
-            <CategoryTitle title="Товары для дома и дачи" />
-            <ul className={styles.cards}>
-                {noSale__goods.slice(0, 3).map(item => {
-                    return <li key={shortid.generate()}><PreviewCard item={item} sale="false"/></li>
-                })}
-            </ul>
-            <CategoryTitle title="Товары для ухода за собой" />
-            <ul className={styles.cards}>
-                {noSale__goods.slice(0, 3).map(item => {
-                    return <li key={shortid.generate()}><PreviewCard item={item} sale="false"/></li>
-                })}
-            </ul>
+            <div className={styles.category__slider}>
+                <CategoryTitle title="Скидки" />
+                <ul className={styles.cards}>
+                    {sale__goods.slice(0, 3).map(item => {
+                        return <li key={shortid.generate()}><PreviewCard item={item} sale="true" /></li>
+                    })}
+                </ul>
+                <button className={styles.category__slider__button} />
+            </div>
+
+            <div className={styles.category__slider}>
+                <CategoryTitle title="Посуда, кухонный принадлежности" />
+                <ul className={styles.cards}>
+                    {noSale__goods.slice(0, 3).map(item => {
+                        return <li key={shortid.generate()}><PreviewCard item={item} sale="false" /></li>
+                    })}
+                </ul>
+                <button className={styles.category__slider__button} />
+            </div>
+
+            <div className={styles.category__slider}>
+                <CategoryTitle title="Бытовая техника для кухни" />
+                <ul className={styles.cards}>
+                    {noSale__goods.slice(0, 3).map(item => {
+                        return <li key={shortid.generate()}><PreviewCard item={item} sale="false" /></li>
+                    })}
+                </ul>
+                <button className={styles.category__slider__button} />
+            </div>
+
+            <div className={styles.category__slider}>
+                <CategoryTitle title="Бытовая химия" />
+                <ul className={styles.cards}>
+                    {noSale__goods.slice(0, 3).map(item => {
+                        return <li key={shortid.generate()}><PreviewCard item={item} sale="false" /></li>
+                    })}
+                </ul>
+                <button className={styles.category__slider__button} />
+            </div>
+
+            <div className={styles.category__slider}>
+                <CategoryTitle title="Хозяйственные товары" />
+                <ul className={styles.cards}>
+                    {noSale__goods.slice(0, 3).map(item => {
+                        return <li key={shortid.generate()}><PreviewCard item={item} sale="false" /></li>
+                    })}
+                </ul>
+                <button className={styles.category__slider__button} />
+            </div>
+
+            <div className={styles.category__slider}>
+                <CategoryTitle title="Товары для дома и дачи" />
+                <ul className={styles.cards}>
+                    {noSale__goods.slice(0, 3).map(item => {
+                        return <li key={shortid.generate()}><PreviewCard item={item} sale="false" /></li>
+                    })}
+                </ul>
+                <button className={styles.category__slider__button} />
+            </div>
+
+            <div className={styles.category__slider}>
+                <CategoryTitle title="Товары для ухода за собой" />
+                <ul className={styles.cards}>
+                    {noSale__goods.slice(0, 3).map(item => {
+                        return <li key={shortid.generate()}><PreviewCard item={item} sale="false" /></li>
+                    })}
+                </ul>
+                <button className={styles.category__slider__button} />
+            </div>
         </section>
     );
 }
